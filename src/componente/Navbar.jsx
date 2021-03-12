8be8462c704527f1e4f5793dcae73a4f80c25d9f
badoco2 <alexandre.badoco@hotmail.com>
2021-03-11 23:13:19 -0300

    1.4

--- src/componente/Navbar.jsx
+++ src/componente/Navbar.jsx
@@ -4,6 +4,9 @@ import Presente from '../assets/presente.png';
 import Estrela from '../assets/star.png';
 import Bag from '../assets/bag.svg';
 import Icons from './icon';
+import Avatar from '../assets/avatar.png';
+
+
 
 export default function navbar(){
     return(
@@ -25,24 +28,46 @@ export default function navbar(){
                         <div className="w3-third w3-mobile">
                             <div className="w3-bar-item w3-mobile"><h2>BEST TOUR PLAN</h2></div>
                         </div>
-                        <div className="w3-third w3-margin-top">
+                        <div className="w3-third w3-margin-top w3-hide-small">
                             <input className="w3-input w3-left w3-threequarter w3-round w3-light-grey" type="text" placeholder="Qual é o seu destino?"/>
                            <a href=""><div className="w3-rest w3-border w3-center w3-padding w3-red w3-round"><i className="fa fa-search w3-large"></i></div></a> 
                         </div>
-                        <div className="w3-third w3-margin-top">
+                        <div className="w3-third w3-margin-top w3-hide-small">
                             <div className="w3-third w3-bar" >
-                                <img src={Bag} style={{width: 65}} className="w3-bar-item w3-right" alt=""/>
+                            <i className="material-icons" style={{fontSize:40, marginInline: 35}}>local_grocery_store 
+                            </i>
+                            
                             </div>  
-                            <div className="w3-twothird w3-bar">
-                                
-                            </div>
-                             
+                            <div className="w3-twothird w3-bar w3-hide-small ">
+                                <a href="#">
+                                <i className="material-icons w3-right" style={{marginTop: 8}}>arrow_drop_down</i>
+                                <h3 className="w3-right" style={{marginTop: -10, paddingTop:10, paddingLeft: 5}}>Alexandre </h3>
+                                <img src={Avatar} alt="Avatar" className="w3-right w3-circle" style={{width:40}}/>    
+                                </a>
+                            </div>   
                         </div>
                     </div>
                 </div>
-                <div className="w3-row darkblue w3-padding-16">
+                <div className="w3-row darkblue w3-padding">
+                
+
+
+                    <div className=" w3-content w3-bar w3-mobile">
+                        
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>OFERTAS</h5></div>
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>HOTELS</h5></div>
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>ATIVIDADES</h5></div>
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>DIARIAS</h5></div>
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>EVENTOS</h5></div>
+                        <div className="w3-col w3-bar-item w3-l2 w3-mobile menu"><h5>RESTAURANTES</h5></div>
+                        
+                    </div>
                 </div>
             </header>
+            <div className="w3-col">
+                <div className="w3-content w3-red">i</div>
+                <div className="w3-row"></div>
+            </div>
         </div>
     )
-}
+}   
